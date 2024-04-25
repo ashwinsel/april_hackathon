80991d9fa43a64b09d7c81c9ea09f4cd1bfe2b32
KTC96 <kyleclow96@gmail.com>
2024-04-25 20:36:48 +0000

    change url name

--- home/urls.py
+++ home/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
-    path('contact_confirmation/', views.contact, name='contact_confirmation'),
+    path('contact_confirmation/', views.contactConfirmation, name='contact_confirmation'),
     path('about_us/', views.aboutUs, name='about us'),
     path('learn/', views.learn, name='learn'),
     path('endometriosis/', views.endometriosis, name='endometriosis'),
