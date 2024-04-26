d2cda808bef927d2d6ce3c080d98b9effe9bc105
KTC96 <kyleclow96@gmail.com>
2024-04-26 12:52:11 +0000

    heroku change

--- april_hackathon/settings.py
+++ april_hackathon/settings.py
@@ -104,9 +104,9 @@ WSGI_APPLICATION = 'april_hackathon.wsgi.application'
 #             'NAME': BASE_DIR / 'db.sqlite3',
 #         }
 #     }
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
-    }
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 
 # Password validation
